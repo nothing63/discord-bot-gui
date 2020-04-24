92d56d8579aaed988a42150b6ab3a2cc144cf640
Michael <michalp2404@onet.pl>
2020-04-24 11:30:56 +0200

    2 process not working at the same time

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import discord
+from tkinter import *
+import _thread
+
+tk=Tk()
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -11,12 +15,27 @@ class MyClient(discord.Client):
         super().__init__(*args, **kwargs)
         self.bg_task = self.loop.create_task(self.my_background_task())
 
+    def x(arg=None):
+        global m
+
+        m = str(enterM.get())
+        print(m)
+        enterM.delete(first=0, last=END)
+
     async def my_background_task(self):
         await self.wait_until_ready()
         channel = self.get_channel(494160969107505156) # Channel ID
         while not self.is_closed():
-            x = input("x: ")
-            await channel.send(x)
+            #x = input("x: ")
+            await channel.send(m)
+
+enterM = Entry(tk, width=20)
+enterM.focus()
+enterM.bind("<Return>",MyClient.x)
+enterM.pack()
 
 client = MyClient()
-client.run('token')
+
+tk.mainloop()
+
+client.run('NzAyOTg3MDQ1NjA5NTM3NjE3.XqKlSw.RVULHNTCN0vF7iAvit9INd1s2Pk')
